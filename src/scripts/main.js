fd88531529b2c3c6a5dc7703f740b9cd7dcb1706
Evgeniy <charizardik64@gmail.com>
2025-02-27 16:10:33 +0300

    playbill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
+function getPlaybillData(data) {
+  return data.map(playbill => {
+    return `<a class="playbill__grid-items playbill-card" href="">
+                    <div class="playbill-card__img">
+                        <img src="${playbill.playbillLink}" alt="">
+                    </div>
+
+                    <div class="playbill-card__fade"></div>
+
+                    <div class="playbill-card__content">
+                        <div class="playbill-card__top">
+                            <div class="playbill-card__details">
+                                <div class="playbill-card__details-item">${playbill.details[0]}</div>
+                                <div class="playbill-card__details-item">${playbill.details[1]}</div>
+                            </div>
+                            
+                            ${playbill.isPremiere? '<div class="playbill-card__tag">Премьера</div>' : '' } 
+                        </div>
+                        
+                        <div class="playbill-card__bottom">
+                            <h3 class="playbill-card__title">${playbill.title}</h3>
+                            <div class="playbill-card__genre">
+                                <span class=""><img src="src/assets/theaters.svg" style="rotate: 90deg" alt=""></span>
+                                <p>${playbill.genres.join(', ')}</p>
+
+                            </div>
+                            <div class="playbill-card__duration">
+                                <span class=""><img src="src/assets/timer.svg" alt=""></span>
+                                <p>${playbill.duration}</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>`
+  }).join(' ')
+}
+
 function getNewsData(data) {
   return data.map(news => {
     return `<div class="news-slider__slide news-card swiper-slide">
@@ -33,10 +69,6 @@ function getNewsData(data) {
   }).join(' ')
 }
 
-function renderNewSection(newsData) {
-  document.querySelector('.news-slider__container').innerHTML = newsData
-}
-
 function getFooterData(data) {
   return data.map((column) => {
     return `<div class="footer__column footer-column">
@@ -52,9 +84,21 @@ function getFooterData(data) {
   }).join(' ')
 }
 
+
+
+function renderPlaybill(playbillData) {
+  document.querySelector('.playbill__grid').innerHTML = playbillData
+}
+
+function renderNewSection(newsData) {
+  document.querySelector('.news-slider__container').innerHTML = newsData
+}
+
 function renderFooter(footerData) {
   document.querySelector('.footer__navigation').innerHTML = footerData
 }
+
+
 
 const footerData = [
   {
@@ -409,13 +453,212 @@ const newsData = [
   }
 ]
 
+const movieData = [
+  {
+    _id: '0',
+    title: 'Пророк. История Александра Пушкина',
+    playbillLink: 'src/assets/playbills/dTlaGdWjU8HBAiA.webp',
+    description: 'Пушкин молод, дерзок и любим публикой: он — звезда любого бала, поклонники носят его на руках, а девушки мечтают о его внимании. Но ни высокие покровители, ни верные друзья, ни известность не могут отвести от него злой рок — ссылки, дуэли, безденежье. Пока однажды он не встречает ту самую, единственную, кто осветит его жизнь и станет его судьбой.',
+    details: ['16+', 'RU', '2D', 'Dolby Digital'],
+    genres: ['драма', 'мюзикл'],
+    duration: '2ч 25мин',
+    isPremiere: false,
+    exhibition: {
+      start: '13 февраля',
+      end: '5 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Феликс Умаров',
+    cast: ['Юра Борисов', 'Алена Долголенко'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1738832817078-122.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1738832832806-123.jpeg'
+      },
+      {
+        _id: '3',
+        link: 'src/assets/shots/1738832860118-124.jpeg'
+      },
+    ],
+  },
+  {
+    _id: '1',
+    title: 'Капитан Америка: Новый мир',
+    playbillLink: 'src/assets/playbills/1hPf3BGJys8pcYzuDkf.webp',
+    description: 'Новый президент США Таддеус Росс решает назначить Капитана Америку на официальную должность. Но в прошлом между ними были разногласия, и Сэм сомневается, что это хорошая идея. Этим же вечером на президента совершается покушение. Уилсон уверен, что предатель скрывается среди ближайшего окружения Росса.',
+    details: ['16+', 'RU', "3D", '2D', 'Dolby Digital'],
+    genres: ['боевик', 'фантастика'],
+    duration: '2ч 5мин',
+    isPremiere: true,
+    exhibition: {
+      start: '27 февраля',
+      end: '12 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Джулиус Она',
+    cast: ['Харрисон Форд', 'Энтони Маки'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1738846053980-162.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1738846080861-163.jpeg'
+      },
+    ],
+  },
+  {
+    _id: '2',
+    title: 'Ущелье',
+    playbillLink: 'src/assets/playbills/9h89MLU3nHiI6fa6keX.webp',
+    description: 'Двое снайперов из элитных спецотрядов выполняют загадочную и опасную миссию. Им поручено охранять разные стороны глубокого, непроходимого ущелья. При этом никто из них не догадывается, что находится на дне. В этих суровых, смертельно опасных условиях двое находят друг в друге родственную душу.',
+    details: ['16+', 'RU', '2D', 'Dolby Digital'],
+    genres: ['боевик', 'ужасы', 'фантастика'],
+    duration: '2ч 15мин',
+    isPremiere: false,
+    exhibition: {
+      start: '20 февраля',
+      end: '5 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Скотт Дерриксон',
+    cast: ['Аня Тейлор-Джой', 'Майлз Теллер'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1738844173824-149.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1738844187334-150.jpeg'
+      },
+      {
+        _id: '2',
+        link: 'src/assets/shots/1738844202220-151.jpeg'
+      },
+    ],
+  },
+  {
+    _id: '3',
+    title: 'Капитан Крюк',
+    playbillLink: 'src/assets/playbills/sb4LeIPUgum6eX.webp',
+    description: 'Никита Крюков — в прошлом успешный хоккеист, закончивший спортивную, а затем и агентскую карьеру из-за своего неуправляемого гнева. С ним никто не готов работать. Внезапно ему приходит сообщение о смерти мамы. Он срывается в родной город. Но оказывается, что это сообщение отправила младшая дочь мамы от второго мужа. После смерти отца Никита почти сразу уехал в Москву, а затем в Америку и все это время не общался с мамой — он не смог простить того, что она почти сразу сошлась с другим.',
+    details: ['12+', 'RU', '2D', 'Dolby Digital'],
+    genres: ['драмма', 'комедия'],
+    duration: '1ч 40мин',
+    isPremiere: false,
+    exhibition: {
+      start: '20 февраля',
+      end: '5 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Максим Максимов',
+    cast: ['Роман Курцын', 'Ева Смирнова'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1738842040476-143.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1738842054704-144.jpeg'
+      },
+      {
+        _id: '2',
+        link: 'src/assets/shots/1738842088304-145.jpeg'
+      },
+      {
+        _id: '3',
+        link: 'src/assets/shots/1738842103485-146.jpeg'
+      },
+    ],
+  },
+  {
+    _id: '4',
+    title: 'Обезьяна',
+    playbillLink: 'src/assets/playbills/1eLmK5f2BXESl.webp',
+    description: 'Представьте себе артефакт, с помощью которого можно убить любого человека! Притом совершенно «случайным» образом. Сколько проблем можно решить с помощью такого загадочного предмета, помещенного в игрушечную обезьянку! Особенно если у ее обладателя проблем больше, чем моральных границ. Но что делать, если такое оружие направлено в вашу сторону?',
+    details: ['18+', 'RU', '2D', 'Dolby Digital'],
+    genres: ['ужасы'],
+    duration: '1ч 45мин',
+    isPremiere: true,
+    exhibition: {
+      start: '27 февраля',
+      end: '12 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Осгуд Перкинс',
+    cast: ['Тео Джеймс', 'Татьяна Маслани'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1740040180759-75.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1740040197371-76.jpeg'
+      },
+      {
+        _id: '2',
+        link: 'src/assets/shots/1740040213263-77.jpeg'
+      },
+      {
+        _id: '3',
+        link: 'src/assets/shots/1740040228221-78.jpeg'
+      },
+    ],
+  },
+  {
+    _id: '5',
+    title: 'Клинер',
+    playbillLink: 'src/assets/playbills/K5f2BXLoSeyLSRhD2Sl.webp',
+    description: 'Группа радикальных экоактивистов захватывает 300 заложников на роскошной вечеринке в небоскребе, чтобы вывести на чистую воду коррумпированную верхушку энергетической компании. В рядах банды находится настоящий безумец, который планирует уничтожить заложников в качестве манифеста анархии. Только хрупкая девушка, оказавшаяся снаружи небоскреба — в прошлом спецназовец, а ныне простая мойщица окон — может спасти людей от гибели.',
+    details: ['16+', 'RU', '2D', 'Dolby Digital'],
+    genres: ['боевик', 'триллер', 'драма'],
+    duration: '1ч 50мин',
+    isPremiere: true,
+    exhibition: {
+      start: '27 февраля',
+      end: '12 марта',
+    },
+    productionYear: '2025',
+    directedBy: 'Мартин Кэмпбелл',
+    cast: ['Дейзи Ридли', 'Клайв Оуэн'],
+    shots: [
+      {
+        _id: '0',
+        link: 'src/assets/shots/1740042489750-92.jpeg'
+      },
+      {
+        _id: '1',
+        link: 'src/assets/shots/1740042506278-93.jpeg'
+      },
+      {
+        _id: '2',
+        link: 'src/assets/shots/1740042521615-94.jpeg'
+      },
+      {
+        _id: '3',
+        link: 'src/assets/shots/1740042542795-95.jpeg'
+      },
+    ],
+  },
+]
+
+
+
 window.onscroll = () => {
   const header = document.querySelector('.header')
   const blurClassName = 'blurred-bg'
 
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     header.classList.add(blurClassName)
-    header.style.height = "80px"
+    header.style.height = "60px"
   } else {
     header.classList.remove(blurClassName)
     header.style.height = "100px"
@@ -430,7 +673,6 @@ playbillSortBtn.forEach(btn => {
   btn.addEventListener('click', (event) => {
     playbillSortBtn.forEach(btn => btn.classList.remove('playbill__sort-btn_active'))
     event.target.classList.add('playbill__sort-btn_active')
-
   })
 })
 
@@ -444,5 +686,7 @@ new Swiper(".news-slider", {
   },
 });
 
+
+renderPlaybill(getPlaybillData(movieData))
 renderNewSection(getNewsData(newsData))
 renderFooter(getFooterData(footerData))
